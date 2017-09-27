af51d9c000a1d6cafe693ff5a28a34625c3884bf
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2017-09-27 11:28:17 +0100

    Configure devise for user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,20 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+
+  config.action_mailer.default_url_options = { :host => 'gds-represent.herokuapp.com/' }
+
+# todo - Configure mailer on Heroku
+#   ActionMailer::Base.smtp_settings = {
+#     :address        => 'smtp.sendgrid.net',
+#     :port           => '587',
+#     :authentication => :plain,
+#     :user_name      => ENV['SENDGRID_USERNAME'],
+#     :password       => ENV['SENDGRID_PASSWORD'],
+#     :domain         => 'heroku.com',
+#     :enable_starttls_auto => true
+# }
+
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.

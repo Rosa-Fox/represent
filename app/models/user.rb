4d393336a57b3c05180d9f1dc73f2a9601bda43d
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2017-09-27 15:43:30 +0100

    Validate email address domain

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ validate :email_domain
+
+  def email_domain
+    domain = email.split("@").last
+    if !email.blank?
+      errors.add(:email, "domain must be: @digital.cabinet-office.gov.uk") if domain != "digital.cabinet-office.gov.uk"
+    end
+  end
 end
